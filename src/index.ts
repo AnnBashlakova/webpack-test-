9ed6dd7d83e08eec44dabcae20594dbb891c3cec
Anna <anna47297@gmail.com>
2022-12-19 16:54:45 +0300

    feat: add new folder

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// import './styles/sass/style.sass';
+// import './styles/sass/style.sass';
+import './styles/sass/style.sass'
+// import './styles/css/style.min.css'
+// import "./style.scss";
+console.log('hello');
