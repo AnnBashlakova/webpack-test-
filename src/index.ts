a6e56c87fc62ab8ff48199a8bc95248d77413fe5
Anna <anna47297@gmail.com>
2022-12-20 15:43:45 +0300

    fix: coorect peth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import './styles/sass/style.sass';
 // import './styles/sass/style.sass';
-import './styles/sass/style.sass'
+import './assets/styles/sass/style.sass'
+
 // import './styles/css/style.min.css'
 // import "./style.scss";
 console.log('hello');
