b2693f46ccb508936fb8bb5789e49599ff138054
Anna <anna47297@gmail.com>
2022-12-21 11:25:01 +0300

    fix: chenge path to bg

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // import './styles/sass/style.sass';
 // import './styles/sass/style.sass';
-import './assets/styles/sass/style.sass'
+// import './assets/styles/sass/style.sass'
 
-// import './styles/css/style.min.css'
+// import './assets/styles/css/style.min.css'
+import './assets/styles/sass/style.sass'
 // import "./style.scss";
 console.log('hello');
